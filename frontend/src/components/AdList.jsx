9ea12f0db8f6fbbb2bca86d7b9deeb2725b211e4
owenboy9 <owenkraus9@gmail.com>
2024-04-04 13:46:15 +0200

    ad updated accordring tp db column names

--- frontend/src/components/AdList.jsx
+++ frontend/src/components/AdList.jsx
@@ -27,13 +27,13 @@ function AdList() {
   return <section>
     <h2 className="current">Aktuella auktioner:</h2>
 
-    {filteredAds.map(ItemCard)}
+    {filteredAds?.map(ItemCard)}
 
   </section>
 
   function ItemCard(ad) {
 
-    const { id, rubrik, kön, ålder, stad, län, sysselsättning, enddate, bids } = ad
+    const { id, headline, gender, age, city, county, occupation, endDate } = ad
 
     const handleOfferButton = () => {
       navigate(`/ad/${id}/bid`)
@@ -43,19 +43,19 @@ function AdList() {
       <div className='itemCard-container' key={id}>
         <div className='ad-info'>
 
-          <h3 className="ad-title"><Link to={`/ad/${id}`}>{rubrik}</Link></h3>
-          <p>En {ålder} år gammal {kön.toLowerCase()}, från {län} <br /> som är {sysselsättning.toLowerCase()} och bor i {stad.toLowerCase()}.</p>&nbsp;
-          <p>Denna annons är aktiv till och med {enddate}.</p>
+          <h3 className="ad-title"><Link to={`/ad/${id}`}>{headline}</Link></h3>
+          <p>En {age} år gammal {gender.toLowerCase()}, från {county} <br /> som är {occupation.toLowerCase()} och bor i {city.toLowerCase()}.</p>&nbsp;
+          <p>Denna annons är aktiv till och med {endDate}.</p>
 
 
         </div>
-        <div className='ad-right'>
+        {/*<div className='ad-right'>
           {bids && bids.includes(activeUser.id) ? (<img className='bidPaddle' src={bidpaddle}></img>) : null}
           {bids ? (<p className="antalBud">Antal bud: {bids.length}</p>) : null}
           <div class="button-container">
             <button onClick={handleOfferButton}>Lägg ett bud</button>
           </div>
-        </div>
+    </div>*/}
       </div>
     )
   }
