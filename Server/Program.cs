00c55c0d19565548addad5e5cbd1bd9cbc811508
root <root@vmi1557407.contaboserver.net>
2024-05-07 10:04:11 +0200

    live server setup

--- Server/Program.cs
+++ Server/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using System.IO;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Microsoft.Extensions.FileProviders;
 
 using Server;
 
@@ -13,7 +14,6 @@ using MySql.Data.MySqlClient;
 
 string connectionString = "server=localhost;uid=root;pwd=mypassword;database=vivimos;port=3306";
 var builder = WebApplication.CreateBuilder(args);
-
 
 try
 {
@@ -26,8 +26,46 @@ try
 
     });
     builder.Services.AddAuthentication().AddCookie("opa23.molez.vivimos");
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+  serverOptions.ListenAnyIP(3000);
+});
+
     var app = builder.Build();
 
+    // Här deklarerar vi två variabler: en som håller i vår PATH till vår dist map/folder.
+// Den andra är en class som håller i distmappen som vi vill att vår backend ska hantera.
+    var distPath = Path.Combine(app.Environment.ContentRootPath, "dist");
+var fileProvider = new PhysicalFileProvider(distPath);
+
+app.UseHttpsRedirection();
+
+// Här deklarerar vi att vår app ska använda sig av vår distmapp alltid som en fallback.
+app.UseDefaultFiles(new DefaultFilesOptions
+{
+  FileProvider = fileProvider,
+  DefaultFileNames = new List<string> { "index.html" }
+});
+
+// Här deklarerar vi att vår app ska hantera statiska filer (vår distmapp)
+app.UseStaticFiles(new StaticFileOptions
+{
+  FileProvider= fileProvider,
+  RequestPath = ""
+});
+
+app.UseRouting();
+
+// MapFallback gör så att när det skrivs in en URL i webläsaren som vår backend inte känner igen, så ska den // skicka tillbaka vår index.html som ligger i dist (dist/index.html) och låta frontend ta hand om routingen.
+// Det är default use case när man jobbar med en SPA ( Single Page Application ) så som ramverket React etc.
+
+app.MapFallback(async context =>
+{
+
+  context.Response.ContentType = "text/html";
+  await context.Response.SendFileAsync(Path.Combine(distPath, "index.html"));
+
+});
     app.MapPost("/ads", Ads.AddAd);
     app.MapPost("/api/auth/register", Auth.Register);
     app.MapPost("/auth/login", Auth.Login);
