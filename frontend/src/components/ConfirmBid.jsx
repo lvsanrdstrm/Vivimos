8f92029831cef9923d907cf25f55c03959a20334
owenboy9 <owenkraus9@gmail.com>
2024-05-09 11:53:53 +0200

    changed bid.jsx to get a <p> confirmation instead of alert

--- frontend/src/components/ConfirmBid.jsx
+++ frontend/src/components/ConfirmBid.jsx
@@ -1,55 +1,47 @@
-import { useParams, useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 
 function ConfirmBid() {
-  const { id } = useParams()
-  const navigate = useNavigate()
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [showConfirmation, setShowConfirmation] = useState(false);
 
-  function submitBid(e) {
-    handleBidClick(navigate, e);
+  async function submitBid(e) {
+    e.preventDefault();
+
+    const requestOptions = {
+      method: e.target.method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'same-origin',
+      body: JSON.stringify("{}"),
+    };
+
+    let result = await fetch(e.target.action, requestOptions);
+
+    if (result.ok) {
+      setShowConfirmation(true);
+      console.log('Confirmation set to true');
+      //navigate('/');
+    } else if (result.status === 400) {
+      alert('Något var fel med din data när vi försökte spara ditt bud');
+    } else if (result.status === 404) {
+      alert('Du måste vara inloggad för att lägga bud');
+    } else {
+      alert('Något gick fel när vi försökte spara ditt bud');
+    }
   }
+
 
   return (
     <form onSubmit={submitBid} method="POST" action={`/api/bids/${id}`}>
       <h2>Bekräfta bud</h2>
       <p>Auktion: {id}</p>
-      <input type="submit" value="Lägg ditt bud"></input>
+      <input type="submit" value="Lägg ditt bud" />
+      {showConfirmation && <p id="sparatBud">Ditt bud har sparats.</p>}
     </form>
-  )
+  );
 }
 
-async function handleBidClick(navigate, e) {
-  e.preventDefault()
-
-  const requestOptions = {
-    method: e.target.method,
-    headers: {
-      'Content-Type': 'application/json',
-
-    },
-    credentials: "same-origin",
-    body: JSON.stringify("{}")
-
-  };
-
-  let result = await fetch(e.target.action, requestOptions);
-
-
-  if (result.ok) {
-    var userData = await result.json();
-    console.log(userData)
-    alert('Ditt bud har sparats.');
-    navigate('/');
-
-  } else if (result.status == 400) {
-    alert("Något var fel med din data när vi försökte spara ditt bud")
-  } else if (result.status == 404) {
-    alert("Du måste vara inloggad för att lägga bud")
-  } else {
-    alert("Något gick fel när vi försökte spara ditt bud")
-  }
-
-
-
-
-}
-export default ConfirmBid
+export default ConfirmBid;
